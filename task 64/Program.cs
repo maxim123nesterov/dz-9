be072948d3084b1ae41dbfa2529bef2ea0f03ee6
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-12-20 14:51:01 +0300

    64

---
+++ task 64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int num(int n)
+{
+   if (0 == n) return n; Console.Write($" {n}");
+   return num(n - 1); 
+   
+}
+
+num(n);
