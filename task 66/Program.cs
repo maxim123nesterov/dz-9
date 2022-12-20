5e16dbc743d18c0fb3cf76e606afd020cef09143
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-12-20 15:24:39 +0300

    66

---
+++ task 66/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m>n)
+{(m,n)=(n,m);}
+
+int num(int m1,int n1)
+{
+   Console.Write($" {(m1)}");
+   if (m1 == n1) return n1;
+   return num(m1 + 1, n1); 
+}
+
+num(m,n);
+Console.WriteLine();
+
+int sum = 0;
+while (m <= n)
+{
+    sum = sum + m;
+    m++;
+}
+Console.Write($"Сумма = {sum}");
